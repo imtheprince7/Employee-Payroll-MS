d82f9fb9dcbba2499b80750fec4ece6e31e3931d
imtheprince7 <princeks3577@gmail.com>
2025-02-28 23:51:31 +0530

    first commit

---
+++ src/test/java/com/Employee_Payroll_MS/EmployeePayrollMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Employee_Payroll_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
